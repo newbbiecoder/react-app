c63eeae9b6afa773be3819edbeabcdf12cba8bfa
Rudraksh <rudrakshkumar2908@gmail.com>
2025-10-17 10:07:02 +0000

    Simple commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function Person() {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const fullName = firstName + ' ' + lastName;
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function handleFirstNameChange(e) {
+        setFirstName(e.target.value);
+    } 
+
+    function handleLastNameChange(e) {
+        setLastName(e.target.value);
+    }
+
+    return (
+        <>
+            <h2>Enter your full name</h2>
+            <label>
+                First Name:{' '}
+                <input 
+                    value={firstName}
+                    onChange={handleFirstNameChange}
+                />
+            </label>
+            
+            <label>
+                Second Name: {' '}
+                <input
+                    value={lastName}
+                    onChange={handleLastNameChange} 
+                />
+            </label>
+
+            <p>
+                Full Name: <b> {' '} {fullName} </b>
+            </p>
+        </>
+    );
 }
-
-export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import RecipeList from './Greeting'
+import App from './App'
+import TodoList from './TodoList'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <TodoList /> */}
+    < App />
+    {/* <RecipeList /> */}
   </StrictMode>,
 )
